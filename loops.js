2cfc3ba292c1c4971aa5723dac6ef9454c4f2c7a
rakhi dawande <rakhi1997dawande@gmail.com>
2019-06-14 07:53:29 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,10 +1,6 @@
 function forLoop(array){
 for (i = 0; i < 25; i++) {
-if(i===1){
-array.push(“I am 1 strange loops.”)
-} else {
 array.push(“I am "+ i +" strange loop.”)
-}
 }
 return array
 }
@@ -12,17 +8,18 @@ return array
 
 function whileLoop(number) {
 while (number > 0) {
-console.log(–number);
+  number= number-1
+console.log(number)
 }
-return “done”;
+return “done”
 }
 
 
 function incrementVariable(num) {
-  num = num - 1;
-  return num;
+  num = num - 1
+  return num
 }
 
 do {
-  console.log("I run once regardless.");
-} while (incrementVariable(num) < num);
+  console.log("I run once regardless.")
+} while (incrementVariable(num) < num)
